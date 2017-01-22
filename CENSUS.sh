05acdb9fc1281383c23810903de90c520dcecc70
Prince of Archives <rich.tijerina@gmail.com>
2017-01-22 03:21:34 -0700

    Update CENSUS.sh

--- CENSUS.sh
+++ CENSUS.sh
@@ -2,4 +2,13 @@
 ############################
 # CENSUS - zdb status parser
 #                 ---cronjob
-my ($pools) = @ARGV;
+pool=${1%/}
+work_dir=${2%/}
+d=$( date +%m%d%y );
+zdb -C > "$work_dir"/$d;
+new_sha=%( sha256 "$work_dir"/$d );
+old_sha=%( cat "work_dir"/lastest );
+if ($new_sha != $old_sha)
+then
+  diff_by_line ""$work_dir"/$d "work_dir"/lastest;
+fi
