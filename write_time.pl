f16dd1b312a89da5b8a091428aa667e30ac12d4b
Prince of Archives <rich.tijerina@gmail.com>
2017-07-24 15:11:53 -0700

    Update and rename write_time.d to write_time.pl

---
+++ write_time.pl
@@ -0,0 +1,15 @@
+#!/usr/local/bin/perl
+my $dtrace = <<END;
+/sbin/dtrace -s '
+  syscall::write:entry
+  /pid == $1/
+  {
+    nsec = timestamp;
+  }
+  syscall::write:return
+  /pid == $1 && nsec != 0/
+  {
+    printf("%d", timestamp - nsec);
+  }
+'
+END
