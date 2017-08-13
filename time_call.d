2dd2757be8ec62149b7d2edb157615b5f1d8b325
Prince of Archives <rich.tijerina@gmail.com>
2017-08-12 21:00:17 -0700

    Update time_call.d

--- time_call.d
+++ time_call.d
@@ -1,1 +1,13 @@
 #!/sbin/dtrace
+syscall:::$2
+/pid == $1/
+callout_execute:::callout_start
+{
+        self->cstart = timestamp;
+}
+
+callout_execute:::callout_end
+{
+
+        @length = quantize(timestamp - self->cstart);
+}
