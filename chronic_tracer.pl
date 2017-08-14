2261da7493fefc9cae584366071b43708ef5107f
Prince of Archives <rich.tijerina@gmail.com>
2017-08-13 23:58:54 -0700

    Update chronic_tracer.pl

--- chronic_tracer.pl
+++ chronic_tracer.pl
@@ -1,28 +1,35 @@
 #!/usr/local/bin/perl
-my ($call, $pid, $log) = @ARGV;
+my ($target, $call, $log) = @ARGV;
 my $x = $$;
-open(my $Lfh, '>>', $log);
-$SIG{HUP} = \&SUICIDE;
-die unless fork();
-if ($x != $$)
+open(my $Tfh, '<', $target)
+my @targ = readline $Tfh; 
+chomp @targ; close $Tfh;
+foreach (@targ)
 {
-  sleep 3600;
-  kill("TERM", $x); # sent CTL-c to dtrae
-}
-my $trace = <<END;
-/sbin/dtrace -s '
-syscall:::$call
-/pid == $pid/
-callout_execute:::callout_start
-{
-        self->cstart = timestamp;
-}
+  my $pid = $_;
+  open(my $Lfh, '>>', $log);
+  $SIG{HUP} = \&SUICIDE;
+  die unless fork();
+  if ($x != $$)
+  {
+    sleep 3600;
+    kill("HUP", $x); # sent CTL-c to dtrace
+  }
+  my $trace = <<END;
+  /sbin/dtrace -s '
+  syscall:::$call
+  /pid == $pid/
+  callout_execute:::callout_start
+  {
+          self->cstart = timestamp;
+  }
 
-callout_execute:::callout_end
-{
+  callout_execute:::callout_end
+  {
 
-        @length = quantize(timestamp - self->cstart);
-}'
-END
+          @length = quantize(timestamp - self->cstart);
+  }'
+  END
+}
 sub SUICIDE
   { print $Lfh "$$ $trace\n"; die; }
